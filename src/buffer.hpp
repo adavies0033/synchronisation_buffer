6a195dd6e13b919282c1d1e249e68a7012c17115
adavies0033 <adavies0033@gmail.com>
2023-07-22 19:32:05 +1000

    CHORE cleaned up for devel on branch for OpenCV CUDA module now CMake is fixed

--- src/buffer.hpp
+++ src/buffer.hpp
@@ -6,6 +6,9 @@
 #include <mutex>
 #include <shared_mutex>
 
+#include <opencv2/highgui.hpp>
+#include <opencv2/cudaimgproc.hpp>
+
 
 #define FRAME_HEIGHT 2048
 #define FRAME_WIDTH 1536
@@ -21,7 +24,7 @@ class ThreadSafeBuffer{
     private:
     // ThreadSafebuffer Attributes
     cv::Mat m_host_buffer;
-    cv::cuda::GpuMat m_host_buffer;
+    cv::cuda::GpuMat m_device_buffer;
     mutable std::shared_mutex m_smtx;
     
     public:
@@ -30,8 +33,6 @@ class ThreadSafeBuffer{
     {   
         // Memory Management placeholder
         std::cout << "Host Heap Memory Allocated {Buffer}" << std::endl;
-        m_buffer_ptr = (int*) malloc(sizeof(int));
-        
         
     }
 
@@ -40,26 +41,7 @@ class ThreadSafeBuffer{
     {
          // Memory Management placeholder
          std::cout << "Host Heap Memory Deallocated {Buffer}" << std::endl;
-         free(m_buffer_ptr);
     }
 
-    // Access the Buffer for from external src
-    int getThreadSafeBuffer()
-    {
-        // Enetr Critical Section as Reader
-        std::shared_lock<std::shared_mutex> lock(m_smtx);
-        return (*m_buffer_ptr);
-        // unlock due to drop out of scope
-    }
-
-
-    // Edit the Buffer for from external src
-    void setThreadSafeBuffer(int arg)
-    {
-        // Enetr Critical Section as Writer
-        std::unique_lock<std::shared_mutex> lock(m_smtx);
-        (*m_buffer_ptr) = arg;
-        // unlock due to drop out of scope
-    }
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,11 @@
 #include "main.hpp"
 #include "buffer.hpp"
-
-#define INITALIZATION_STANDARD 0
-
-#ifdef NDEBUG
-
-#  define assert(condition) ((void)0)
-#else
-#  define assert(condition) /*implementation defined*/
-#endif
+#include <assert.h>
 
 
 void test_getter_and_setter(ThreadSafeBuffer* buffer)
 {
-    int test_case = INITALIZATION_STANDARD;
-    assert( 0 == (*buffer)->getThreadSafeBuffer());
-    (*buffer).setThreadSafeBuffer(test_case);
-    assert( test_case == (*buffer)->getThreadSafeBuffer());
-
-    std::cout << "    - Passed void test_getter_and_setter!" << std::endl;
+    // TODO: test getter and setter
 }
 
 
@@ -32,9 +19,7 @@ void test_getter_and_setter(ThreadSafeBuffer* buffer)
 int main()
 {
     
-    ThreadSafeBuffer buffer(INITALIZATION_STANDARD);
 
-    test_getter_and_setter(&buffer);
 
 
     return 0;
